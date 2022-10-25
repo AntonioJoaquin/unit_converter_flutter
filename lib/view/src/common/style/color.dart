717baf345f15bdeb5f6fb8f9b9a503e96a137d31
Antonio Joaquín Luque <antoniojoaquinluque04@gmail.com>
2022-10-25 19:20:16 +0200

    #9- Change colors

--- lib/view/src/common/style/color.dart
+++ lib/view/src/common/style/color.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/widgets.dart';
 
 abstract class ColorPalette {
-  static const Color primary = Color(0xFFC18686);
-  static const Color background = Color(0xFFFBF6F6);
-  static Color shadow = const Color(0xFFC18686).withOpacity(.25);
+  static const Color primary = Color(0xFFB59ADD);
+  static const Color background = Color(0xFFF8F6FB);
+  static Color shadow = const Color(0xFFB59ADD).withOpacity(.25);
   static const Color content = Color(0xFF302C44);
 }
